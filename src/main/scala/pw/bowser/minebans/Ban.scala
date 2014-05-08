43fc6944a94ac2eb0284f2e1b7a67fa878eee6eb
KingBowser <king@bowser.pw>
2014-05-08 09:55:16 -0500

    added ban model

---
+++ src/main/scala/pw/bowser/minebans/Ban.scala
@@ -0,0 +1,23 @@
+package pw.bowser.minebans
+
+import java.util.Date
+
+/**
+ * Ban model.
+ *
+ * Contains information about a single ban.
+ *
+ * Provides accessors for
+ *  'identifier', which can be a field to represent the ban's ID on the service
+ *  'userid'    which should conventionally be the UUID of the banned player
+ *  'banreason' which should be the reason for the ban
+ *  'date'      date of the ban, if available
+ *  'creator'   userId (or user) that performed the banning, if available
+ *
+ * @author Bowser
+ */
+case class Ban(identifier:  String,
+               userId:      String,
+               banReason:   String,
+               banDate:     Date,
+               creator:     String)
